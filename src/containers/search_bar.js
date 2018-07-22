19b5e52832e695c56187c4add79650387a5d3ce2
jp <jp.san.gabriel01@gmail.com>
2018-07-23 01:34:30 +0800

    Hook up the action creator 'fetchWeather' to the SearchBar container

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
@@ -37,3 +37,10 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+// Hook up the action creator 'fetchWeather' to the SearchBar container
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
