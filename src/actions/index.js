dc34efd8581801778f43eeabb687d4793d306a8b
jp <jp.san.gabriel01@gmail.com>
2018-07-23 01:12:07 +0800

    Add function 'feathWeather' that makes an ajax request to fetch weather data given a city

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,12 @@
 //openweathermap.org api key
 const API_KEY = 'b97ed3e053159cec42b6e04183589524';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FEATCH_WEATHER';
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  return {
+    type: FETCH_WEATHER
+  };
+}
