9fa7af43cc2711c7911d01a0f9c7765bbe26aea4
jp <jp.san.gabriel01@gmail.com>
2018-07-23 01:21:36 +0800

    Use axios in fetchWeather function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 //openweathermap.org api key
 const API_KEY = 'b97ed3e053159cec42b6e04183589524';
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
@@ -5,8 +7,10 @@ const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_K
 export const FETCH_WEATHER = 'FEATCH_WEATHER';
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
 
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
